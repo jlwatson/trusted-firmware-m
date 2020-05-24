61d16d50f979c796f2433833326b120363eafbb6
Jean-Luc Watson <jlwatson@eecs.berkeley.edu>
2020-05-24 08:46:39 -0700

    added hooks to gpio interface for updates

--- interface/include/tfm_gpio_veneers.h
+++ interface/include/tfm_gpio_veneers.h
@@ -11,9 +11,13 @@ extern "C" {
 uint32_t tfm_gpio_read_all(void);				// added by Ryan
 uint8_t tfm_gpio_write_all(uint32_t out);		// added by Ryan
 uint8_t tfm_gpio_enable_outputs(uint32_t pins);	// added by Ryan
+void tfm_gpio_disable_all_outputs(void);
 uint8_t tfm_gpio_read(uint8_t pin);				// added by Ryan
+void tfm_gpio_set_update_hooks(uint32_t trigger, uint32_t apply);
 
 uint8_t tfm_gpio_enable_output(uint8_t pin);
+uint32_t tfm_gpio_output_enabled();
+uint32_t tfm_gpio_dataout();
 uint8_t tfm_gpio_set(uint8_t pin);
 uint8_t tfm_gpio_clear(uint8_t pin);
 uint8_t tfm_gpio_disable(uint8_t pin);
@@ -39,6 +43,8 @@ typedef struct {
 
 uint8_t tfm_gpio_interrupt_enable(uint8_t pin, gpio_int_config *cfg);
 uint8_t tfm_gpio_interrupt_disable(uint8_t pin);
+uint32_t tfm_gpio_interrupts_enabled();
+uint32_t tfm_gpio_interrupt_callback_for_pin(uint8_t pin);
 
 #ifdef __cplusplus
 }
--- secure_fw/ns_callable/tfm_gpio_veneers.c
+++ secure_fw/ns_callable/tfm_gpio_veneers.c
@@ -28,18 +28,30 @@ typedef struct {
     uint32_t intstatusintclr; 
 } gpio_t;
 
+uint32_t interrupts_enabled = 0;
+uint32_t output_enabled = 0;
+
 typedef void __attribute__((cmse_nonsecure_call)) ns_interrupt_cb(void);
+
+typedef uint8_t __attribute__((cmse_nonsecure_call)) ns_update_trigger(uint8_t pin);
+typedef void __attribute__((cmse_nonsecure_call)) ns_update_apply(void);
 
 static gpio_t *gpio = (gpio_t *) MUSCA_GPIO_S_BASE;
 static ns_interrupt_cb *callbacks[N_PINS];
 
-ns_interrupt_cb *gpio_cb = NULL;
+ns_update_trigger *should_trigger_update = NULL;
+ns_update_apply *apply_update = NULL;
 
 // All specific pin handlers fall into this one
 void GPIO_Handler(uint8_t pin) {
     gpio->intstatusintclr = 1 << pin;
 
-    LOG_MSG("GPIO_IRQHandler triggered!");
+    if (should_trigger_update &&
+        apply_update &&
+        should_trigger_update(pin)) {
+        apply_update();
+    }
+
     if (callbacks[pin]) callbacks[pin]();
 }
 
@@ -60,6 +72,11 @@ void GPIO_13_IRQHandler() { GPIO_Handler(13); }
 void GPIO_14_IRQHandler() { GPIO_Handler(14); }
 void GPIO_15_IRQHandler() { GPIO_Handler(15); }
 
+__tfm_secure_gateway_attributes__
+void tfm_gpio_set_update_hooks(uint32_t trigger, uint32_t apply) {
+    should_trigger_update = (ns_update_trigger *)trigger;
+    apply_update = (ns_update_apply *)apply;
+}
 
 /* The following function was added by Ryan */
 __tfm_secure_gateway_attributes__
@@ -84,7 +101,14 @@ uint8_t tfm_gpio_enable_outputs(uint32_t pins) {
     }
     
     gpio->outenset |= pins;
+    output_enabled = pins;
     return 0;
+}
+
+__tfm_secure_gateway_attributes__
+void tfm_gpio_disable_all_outputs(void) {
+    gpio->outenclr = 0xFFFF;
+    output_enabled = 0; 
 }
 
 // reads the value at the given pin
@@ -108,6 +132,7 @@ uint8_t tfm_gpio_enable_output(uint8_t pin) {
     }
 
     gpio->outenset = 1 << pin;
+    output_enabled |= 1 << pin;
 
     return 0;
 }
@@ -122,6 +147,8 @@ uint8_t tfm_gpio_interrupt_enable(uint8_t pin, gpio_int_config *cfg) {
     gpio->intenset = 1 << pin;
     gpio->inttypeset = cfg->type << pin;
     gpio->intpolset = cfg->polarity << pin;
+
+    interrupts_enabled |= 1 << pin;
 
     callbacks[pin] = NULL;
     if (cfg->cb) {
@@ -140,9 +167,31 @@ uint8_t tfm_gpio_interrupt_disable(uint8_t pin) {
     NVIC_DisableIRQ(GPIO_0_IRQn + pin);
     gpio->intenclr = 1 << pin; 
 
+    interrupts_enabled &= ~(1 << pin);
+
     callbacks[pin] = NULL;
 
     return 0;
+}
+
+__tfm_secure_gateway_attributes__
+uint32_t tfm_gpio_interrupt_callback_for_pin(uint8_t pin) {
+    return (uint32_t) callbacks[pin];
+}
+
+__tfm_secure_gateway_attributes__
+uint32_t tfm_gpio_interrupts_enabled() {
+    return interrupts_enabled;
+}
+
+__tfm_secure_gateway_attributes__
+uint32_t tfm_gpio_output_enabled() {
+    return output_enabled;
+}
+
+__tfm_secure_gateway_attributes__
+uint32_t tfm_gpio_dataout() {
+    return gpio->dataout;
 }
 
 __tfm_secure_gateway_attributes__
@@ -172,6 +221,7 @@ uint8_t tfm_gpio_disable(uint8_t pin) {
     }
 
     gpio->outenclr = 1 << pin;
+    output_enabled &= ~(1 << pin);
     return 0;
 }
 
